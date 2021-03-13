7d6d3018a6a842b239191889ca66b5c3c6e44efe
346nbhredb <dmrotek111@gmail.com>
2021-03-13 18:47:59 +0100

    debuging

--- PracowaniaProgramowania/src/main/java/second/shortcuts/ClassThatHaveItAll.java
+++ PracowaniaProgramowania/src/main/java/second/shortcuts/ClassThatHaveItAll.java
@@ -1,6 +1,32 @@
 package second.shortcuts;
 
+import java.util.List;
+
 public class ClassThatHaveItAll implements InterfaceOne {
 
-    final static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(ClassThatHaveItAll.class); String name; Integer number; public void printMe(String info) { logger.info(info); } public void usageOfPrint() { printMe("Hi"); }
+    final static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(ClassThatHaveItAll.class);
+
+    List<Long> list;
+    String name;
+    Integer number;
+
+    public ClassThatHaveItAll(List<Long> list) {
+        this.list = list;
+    }
+
+    public void setList(List<Long> list) {
+        this.list = list;
+    }
+
+    public List<Long> getList() {
+        return list;
+    }
+
+    public void printMe(String info) {
+        logger.info(info);
+    }
+
+    public void usageOfPrint() {
+        printMe("Hi");
+    }
 }
